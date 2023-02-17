fb1e07fb0de16f942a37947077ea6d6e962e9864
michaljedro <michal.jedro@wp.pl>
2023-02-17 20:54:17 +0100

    removing files, add index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-export default function Home() {
-  return (
-    <>
-      <main className={styles.main}>
-        <h1>Section with Routing</h1>
-      </main>
-    </>
-  );
+function Home() {
+  return <h1>Home Page</h1>;
 }
+
+export default Home;
+
